d23e80dd02dc26a5f10e0528fac60d18e372021d
Ketil Grytten <ketil.grytten@adra.com>
2018-08-18 02:22:35 +0200

    Changed Order class to have only one method which created receipt.

--- OrderService.Tests/OrderTests.cs
+++ OrderService.Tests/OrderTests.cs
@@ -5,7 +5,7 @@ namespace OrderService.Tests
     [TestFixture]
     public class OrderTests
     {
-        private static readonly Product MotorSuper= new Product("Car Insurance", "Super", Product.Prices.TwoThousand);
+        private static readonly Product MotorSuper = new Product("Car Insurance", "Super", Product.Prices.TwoThousand);
         private static readonly Product MotorKasko = new Product("Car Insurance", "Kasko", Product.Prices.OneThousand);
 
         [Test]
@@ -13,7 +13,7 @@ namespace OrderService.Tests
         {
             var order = new Order("Test Company");
             order.AddLine(new OrderLine(MotorSuper, 1));
-            var actual = order.GenerateReceipt();
+            var actual = order.GenerateReceipt("text");
             var expected = 
                 $"Order receipt for 'Test Company'\r\n\t1 x Car Insurance Super 1 = kr 2{System.Globalization.NumberFormatInfo.CurrentInfo.NumberGroupSeparator}000,00\r\nSubtotal: kr 2{System.Globalization.NumberFormatInfo.CurrentInfo.NumberGroupSeparator}000,00\r\nMVA: kr 500,00\r\nTotal: kr 2{System.Globalization.NumberFormatInfo.CurrentInfo.NumberGroupSeparator}500,00";
 
@@ -25,7 +25,7 @@ namespace OrderService.Tests
         {
             var order = new Order("Test Company");
             order.AddLine(new OrderLine(MotorSuper, 1));
-            var actual = order.GenerateHtmlReceipt();
+            var actual = order.GenerateReceipt("html");
 
             var expected =
                 $"<html><body><h1>Order receipt for 'Test Company'</h1><ul><li>1 x Car Insurance Super = kr 2{System.Globalization.NumberFormatInfo.CurrentInfo.NumberGroupSeparator}000,00</li></ul><h3>Subtotal: kr 2{System.Globalization.NumberFormatInfo.CurrentInfo.NumberGroupSeparator}000,00</h3><h3>MVA: kr 500,00</h3><h2>Total: kr 2{System.Globalization.NumberFormatInfo.CurrentInfo.NumberGroupSeparator}500,00</h2></body></html>";
@@ -38,7 +38,7 @@ namespace OrderService.Tests
         {
             var order = new Order("Test Company");
             order.AddLine(new OrderLine(MotorKasko, 1));
-            var actual = order.GenerateReceipt();
+            var actual = order.GenerateReceipt("text");
             var expected = 
                 $"Order receipt for 'Test Company'\r\n\t1 x Car Insurance Kasko 1 = kr 1{System.Globalization.NumberFormatInfo.CurrentInfo.NumberGroupSeparator}000,00\r\nSubtotal: kr 1{System.Globalization.NumberFormatInfo.CurrentInfo.NumberGroupSeparator}000,00\r\nMVA: kr 250,00\r\nTotal: kr 1{System.Globalization.NumberFormatInfo.CurrentInfo.NumberGroupSeparator}250,00";
 
@@ -50,10 +50,23 @@ namespace OrderService.Tests
         {
             var order = new Order("Test Company");
             order.AddLine(new OrderLine(MotorKasko, 1));
-            var actual = order.GenerateHtmlReceipt();
+            var actual = order.GenerateReceipt("html");
 
             var expected =
                 $"<html><body><h1>Order receipt for 'Test Company'</h1><ul><li>1 x Car Insurance Kasko = kr 1{System.Globalization.NumberFormatInfo.CurrentInfo.NumberGroupSeparator}000,00</li></ul><h3>Subtotal: kr 1{System.Globalization.NumberFormatInfo.CurrentInfo.NumberGroupSeparator}000,00</h3><h3>MVA: kr 250,00</h3><h2>Total: kr 1{System.Globalization.NumberFormatInfo.CurrentInfo.NumberGroupSeparator}250,00</h2></body></html>";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void can_generate_json_receipt_for_motor_kasko()
+        {
+            var order = new Order("Test Company");
+            order.AddLine(new OrderLine(MotorKasko, 1));
+            var actual = order.GenerateReceipt("json");
+        
+            var expected =
+                "{    order-receipt-for: 'Test Company',{quantity:1,product-type: Car Insurance,product:{name: Kasko, price: kr 1 000,00}}    subtotal: kr 1 000,00,    mva: kr 250,00,    total: kr 1 250,00}";
 
             Assert.AreEqual(expected, actual);
         }
--- OrderService/Order.cs
+++ OrderService/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OrderService.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,76 +22,17 @@ namespace OrderService
             _orderLines.Add(orderLine);
         }
 
-        public string GenerateReceipt()
+        public IList<OrderLine> OrderLines
         {
-            var totalAmount = 0d;
-            var result = new StringBuilder($"Order receipt for '{Company}'{Environment.NewLine}");
-            foreach (var line in _orderLines)
-            {
-                var thisAmount = 0d;
-                switch (line.Product.Price)
-                {
-                    case Product.Prices.OneThousand:
-                        if (line.Quantity >= 5)
-                            thisAmount += line.Quantity * line.Product.Price * .9d;
-                        else
-                            thisAmount += line.Quantity * line.Product.Price;
-                        break;
-                    case Product.Prices.TwoThousand:
-                        if (line.Quantity >= 3)
-                            thisAmount += line.Quantity * line.Product.Price * .8d;
-                        else
-                            thisAmount += line.Quantity * line.Product.Price;
-                        break;
-                }
-                result.AppendLine(
-                    $"\t{line.Quantity} x {line.Product.ProductType} {line.Product.ProductName} {line.Quantity} = {thisAmount:C}");
-                totalAmount += thisAmount;
-            }
-            result.AppendLine($"Subtotal: {totalAmount:C}");
-            var totalTax = totalAmount * Product.Prices.TaxRate;
-            result.AppendLine($"MVA: {totalTax:C}");
-            result.Append($"Total: {(totalAmount + totalTax):C}");
-            return result.ToString();
+            get { return _orderLines; }
         }
 
-        public string GenerateHtmlReceipt()
+        public string GenerateReceipt(string format)
         {
-            var totalAmount = 0d;
-            var result = new StringBuilder($"<html><body><h1>Order receipt for '{Company}'</h1>");
-            if (_orderLines.Any())
-            {
-                result.Append("<ul>");
-                foreach (var line in _orderLines)
-                {
-                    var thisAmount = 0d;
-                    switch (line.Product.Price)
-                    {
-                        case Product.Prices.OneThousand:
-                            if (line.Quantity >= 5)
-                                thisAmount += line.Quantity * line.Product.Price * .9d;
-                            else
-                                thisAmount += line.Quantity * line.Product.Price;
-                            break;
-                        case Product.Prices.TwoThousand:
-                            if (line.Quantity >= 3)
-                                thisAmount += line.Quantity * line.Product.Price * .8d;
-                            else
-                                thisAmount += line.Quantity * line.Product.Price;
-                            break;
-                    }
-                    result.Append(
-                        $"<li>{line.Quantity} x {line.Product.ProductType} {line.Product.ProductName} = {thisAmount:C}</li>");
-                    totalAmount += thisAmount;
-                }
-                result.Append("</ul>");
-            }
-            result.Append($"<h3>Subtotal: {totalAmount:C}</h3>");
-            var totalTax = totalAmount * Product.Prices.TaxRate;
-            result.Append($"<h3>MVA: {totalTax:C}</h3>");
-            result.Append($"<h2>Total: {(totalAmount + totalTax):C}</h2>");
-            result.Append("</body></html>");
-            return result.ToString();
+            IReciept reciept = RecieptFabric.CreateRecieptProvider(format);
+            return reciept.GenerateReceipt(this);
+
         }
+
     }
 }
